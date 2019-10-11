1952d4cd16d37906b2b319199ea5befe34e4e396
鎌田　拓臣 <yuuteayuu335dat@gmail.com>
2019-10-11 16:11:56 +0900

    a

--- Assets/stage/script/Enemy/Enemy1.cs
+++ Assets/stage/script/Enemy/Enemy1.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Enemy1 : MonoBehaviour
 {
+    int speed=3;
+    R
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = transform.position;
+
     }
 
     // Update is called once per frame
